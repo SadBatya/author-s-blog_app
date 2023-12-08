d275bc9219de77b66ec8d356414724b6eb5d6581
sadbatya <sadbatya@mail.ru>
2023-12-08 16:13:24 -0500

    start project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  text-align: center;
+`
+function App() {
+  return (
+    <>
+      <div>hello</div>
+      <i className="fa fa-address-book" ></i>
+      <Div>Hello 2</Div>
+    </>
+  );
+}
+
+export default App;
