4956a45e50f40b32e32e52cf678ced859a68731d
sadbatya <sadbatya@mail.ru>
2023-12-25 14:34:28 -0500

    some problems

--- src/bff/get-user.js
+++ src/bff/get-user.js
@@ -1,19 +1,4 @@
-export const getUser = async (loginToFind) => {
-  try {
-    const response = await fetch(`http://localhost:3005/users?login=${loginToFind}`);
-
-    if (!response.ok) {
-      throw new Error('Failed to fetch user data');
-    }
-
-    const loadedUsers = await response.json();
-
-    if (loadedUsers.length > 0) {
-      return loadedUsers[0];
-    } else {
-      throw new Error('User not found');
-    }
-  } catch (error) {
-    console.error('Error:', error);
-  }
-};
+export const getUser = async (loginToFind) => 
+  fetch(`http://localhost:3005/users/login=${loginToFind}`).then((loadedUsers) =>
+    loadedUsers.json()[0])
+.then(([loadedUser]) => loadedUser);
